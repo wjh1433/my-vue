414416139e69cf42d0fd5a14b62e10f851d8431e
wjh1433 <403381306@qq.com>
2025-11-20 19:19:08 +0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'prettier'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {}
+}
